1ac0c089da2e82c03b6d13bf853e0d27318507f2
congminh07 <duongcongminh63@gmail.com>
2025-11-04 10:53:57 +0700

    ex43

---
+++ ex43/Main.java
@@ -0,0 +1,13 @@
+package ex43;
+
+
+    public class Main {
+        public static void main(String[] args) {
+            Point2D p2 = new Point2D(3.5f, 2.0f);
+            System.out.println(p2); // (3.5,2.0)
+
+            Point3D p3 = new Point3D(3.5f, 2.0f, 5.0f);
+            System.out.println(p3); // (3.5,2.0,5.0)
+        }
+    }
+
